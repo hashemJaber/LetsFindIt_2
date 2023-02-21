146510a17acb881cd551df0a50dfacc109b10b55
hashemJaber <91439032+hashemJaber@users.noreply.github.com>
2023-02-21 13:41:31 -0800

    adding the filter object

--- App.js
+++ App.js
@@ -12,11 +12,29 @@ export default function App() {
   const [ref,setRef]=React.useState(false);
    React.useEffect(()=>{
    setRef(!ref);
+
    
-  },[strore.items.length])
-  const [filter,setFilter]=React.useState({item});
+  },[strore.items.length]);
   
-  const [globalRef,setGloabalRef]=React.useState(false);
+/*
+{
+  title: 'Lala',
+  price: 2,
+  categoryId: 4,
+  description: 'E32ere3r',
+  date_of_posting: 1677015050,
+  images: [
+    {
+      imageUri: 'file:///Users/hashemjaber/Library/Developer/CoreSimulator/Devices/6E51698F-A8BF-49BF-BF41-24146BA1B149/data/Containers/Data/Application/A19DB979-9062-4091-940F-02BADE2CA8C8/Library/Caches/ExponentExperienceData/%2540anonymous%252FSELLIT-a98a92d5-d231-4fe1-a190-c679bb6a4e42/ImagePicker/F2F239DB-111A-470B-B0B4-8DAA55E1C60B.jpg'
+    }
+  ],
+  location: { latitude: 38.57873, longitude: -121.466786 }
+}
+*/
+
+const [filter,setFilter]=React.useState({title:null,price:null,categoryId:null,description:null,date_of_posting:null,location:{latitude:null,longitude:null}});
+
+const [globalRef,setGloabalRef]=React.useState(false);
 const [user,setUser]=React.useState(authStorage.getToken());
 const [refresh,setRefresh]=React.useState(false);
 setTimeout(()=>{
@@ -42,7 +60,6 @@ const styles = StyleSheet.create({
     
  
   
-    
   },
 
 });
